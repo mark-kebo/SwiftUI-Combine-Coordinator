2cbcc08c46d71e2ac130526605687739ed877d7a
Dmitry Vorozhbicki <dima.worozbicki@gmail.com>
2022-02-16 10:44:11 +0100

    init project

---
+++ SwiftUI-Combine-Coordinator/SwiftUI_Combine_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_CoordinatorApp.swift
+//  SwiftUI-Combine-Coordinator
+//
+//  Created by Dmitry Vorozhbicki on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
